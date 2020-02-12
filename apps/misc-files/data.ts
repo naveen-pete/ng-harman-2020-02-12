655c430123d2201efe06737a815f2f72d4e0f28b
naveen-pete <naveen.shenoy.blr@gmail.com>
2020-02-12 13:48:38 +0530

    commit first app and misc folder into the repo

---
+++ apps/misc-files/data.ts
@@ -0,0 +1,14 @@
+export const users = [
+  { id: 1, name: 'ram' },
+  { id: 2, name: 'hari' },
+  { id: 3, name: 'krish' }
+];
+
+export const posts = [
+  { id: 1, userId: 1, title: 'ram-p1' },
+  { id: 2, userId: 1, title: 'ram-p2' },
+  { id: 3, userId: 2, title: 'hari-p1' },
+  { id: 4, userId: 2, title: 'hari-p2' },
+  { id: 5, userId: 3, title: 'krish-p1' },
+  { id: 6, userId: 3, title: 'krish-p2' }
+];
