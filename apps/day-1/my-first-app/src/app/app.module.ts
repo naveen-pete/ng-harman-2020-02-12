2f6ada30a48e6549cb347e012e1abeee4ca504c1
naveen-pete <naveen.shenoy.blr@gmail.com>
2020-02-12 17:46:10 +0530

    commit code at the end of day 2

--- apps/day-1/my-first-app/src/app/app.module.ts
+++ apps/day-1/my-first-app/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule
